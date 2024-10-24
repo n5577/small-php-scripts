df9d8619e0df114700cfe9f620fb9fe51e9c154f
Nick <42870962+n5577@users.noreply.github.com>
2024-10-24 10:58:52 -0700

    Update scan-list-link.php
    cleanup

--- scan-list-link.php
+++ scan-list-link.php
@@ -1,6 +1,5 @@
-// simple script that scans a directory and links all of the files of a few extensions
-
 <?php
+// scan a directory and list links to all of the files for a few extensions
 $path    = './';
 $documents = preg_grep('~\.(pdf|docx|txt|rtf|doc)$~i', scandir($path));
   foreach($documents as $document){
